246558587cf119b0adb0053f0dd8218dbff902e7
Timothy Volpe <timothy.volpe@gmail.com>
2011-01-29 14:55:19 -0500

    preliminary audio mechanic added

--- game/init.js
+++ game/init.js
@@ -11,13 +11,24 @@ var ALESTest = Klass.extend({
 
     gbox.addBundle({ file: 'bundle.js?' + timestamp() });
 
+	//var isMusicOn = true;
     $aki.controls.watchKeys({
       B: function() {
         console.log('B pressed');
       },
       C: function() {
         console.log('C pressed');
-      }
+      },
+ 	  D: function() {
+		console.log('D was: ' + isMusicOn);
+		isMusicOn = !isMusicOn;
+		if (isMusicOn)
+		  //gbox.playAudio('bgmix', 'bgmix');
+		  gbox.setAudioUnmute('bggtr');
+		else
+		  //gbox.stopAudio('bgmix');
+		  gbox.setAudioMute('bggtr');
+	  } 
     });
 
     // The 'main' function is registered as a callback: this just says that when we're done with loadAll we should call 'main'
